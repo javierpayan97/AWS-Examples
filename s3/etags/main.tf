0e95816cc2fdc603c681e39258b4dc8dee286f72
Javier Payan <JEPM210997@gmail.com>
2024-10-02 02:18:41 +0000

    removing unneccesary files

--- s3/etags/main.tf
+++ s3/etags/main.tf
@@ -18,5 +18,5 @@ resource "aws_s3_bucket_object" "object" {
   bucket = aws_s3_bucket.default.id
   key    = "myfile.txt"
   source = "myfile.txt"
-  #etag = filemd5("myfile.txt")
+  etag = filemd5("myfile.txt")
 }
